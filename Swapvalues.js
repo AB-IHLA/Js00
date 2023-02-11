c573939eda867f10fa60e70c74949a4f9669facf
ibrahimabdramane62@gmail.com <ibrahimabdramane62@gmail.com>
2023-02-11 13:30:12 +0100

    Js00

--- Swapvalues.js
+++
@@ -1,5 +0,0 @@
-var a=10, b=20, c;
-c=a, a=b, b=c;
-console.log ("a=", a);
-console.log ("b=", b);
-
---
+++ swapValues.js
@@ -0,0 +1,4 @@
+var a=10, b=20, c;
+c=a, a=b, b=c;
+console.log ("New value of a=", a);
+console.log ("New value of b=", b);
